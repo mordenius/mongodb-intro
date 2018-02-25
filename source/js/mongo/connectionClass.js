b5916cc052344a6e75191697e871ff57767f08a4
mordenius <mordenius@gmail.com>
2018-02-25 21:14:30 +0200

    fix(~/mongo/connectionClass) inheritance method calling

--- source/js/mongo/connectionClass.js
+++ source/js/mongo/connectionClass.js
@@ -4,10 +4,8 @@ export default class ConnectionClass {
 	static async basicAction() {
 		try {
 			await ConnectionClass.connect();
-			await ConnectionClass.action();
+			await this.action();
 			await ConnectionClass.disconnect();
-
-			global.console.log("MongoDB: `mongodb-intro` database is initialized");
 		} catch (e) {
 			global.console.error(e);
 		}
